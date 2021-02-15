e49a7e6767b9996f2b8cb554811a5bb4e66bdddb
Andrey Konnov <1and.konn@gmail.com>
2021-02-15 12:02:48 +0400

    updated YM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import {useEffect} from 'react';
 import {useRouter} from 'next/router';
 import {NextWebVitalsMetric} from 'next/dist/next-server/lib/utils';
 import {AnimateSharedLayout} from 'framer-motion';
-import ym, {YMInitializer} from 'react-yandex-metrika';
 
 import Layout from '@/layout/layout';
 
@@ -29,28 +28,8 @@ const App: React.FC<PropsType> = ({Component, pageProps}) => {
     };
   }, [router.events]);
 
-  useEffect(() => {
-    // To hit only in production and only on client side (in browser)
-    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
-      const url = window.location.pathname + window.location.search;
-      ym('hit', url);
-    }
-  }, []);
-
   return (
     <>
-      {process.env.NODE_ENV === 'production' && (
-        <YMInitializer
-          accounts={[parseInt(process.env.YM_COUNTER_ID as string, 10)]}
-          options={{
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true,
-          }}
-          version="2"
-        />
-      )}
       <AnimateSharedLayout>
         <Provider value={rootStore}>
           <Layout>
