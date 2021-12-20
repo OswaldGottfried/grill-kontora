f6c3af6b175d14043aa3025afe71eb6e0221b989
Andrey Konnov <1and.konn@gmail.com>
2021-12-20 08:46:45 +0300

    change name of varibles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -74,7 +74,7 @@ class MyDocument extends Document {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window,document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', ${process.env.PIXEL_IDs}); 
+              fbq('init', ${process.env.PIXEL_ID}); 
               fbq('track', 'PageView');
             `,
             }}
